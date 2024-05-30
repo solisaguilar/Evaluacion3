85f37438856cf46b46f25dc0967e57d0601763f2
Orlando <sa23004@esfe.agape.edu.sv>
2024-05-30 09:02:45 -0600

    Código

--- src/evalucionTres/Cliente.java
+++ src/evalucionTres/Cliente.java
@@ -1,2 +1,50 @@
-package evalucionTres;public class Cliente {
+package evalucionTres;
+
+public class Cliente {
+    private int Id;
+    private String codigo;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+
+    public Cliente() {
+
+    }
+
+    // Constructor
+    public Cliente(int id, String codigo, String nombre, String apellido, String email, String telefono) {
+        Id = id;
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
 }
+
--- src/evalucionTres/RegistroDeClientes.java
+++ src/evalucionTres/RegistroDeClientes.java
@@ -1,2 +1,50 @@
-package evalucionTres;public class RegistroDeClientes {
+package evalucionTres;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class RegistroDeClientes {
+    public static void main(String[] args) {
+        ArrayList<Cliente> listaCliente = new ArrayList<>();
+        Scanner datos = new Scanner(System.in);
+        int id = 1;
+        String decision;
+
+        do {
+
+            System.out.println("Ingrese el id del cliente");
+            String Id = datos.nextLine();
+
+            System.out.println("Ingrese el codigo del cliente");
+            String codigo = datos.nextLine();
+
+            System.out.println("Ingrese el nombre del cliente: ");
+            String nombre = datos.nextLine();
+
+            System.out.println("Ingrese el apellido del cliente: ");
+            String apellido = datos.nextLine();
+
+            System.out.println("Ingrese el email del cliente: ");
+            String email = datos.nextLine();
+
+            System.out.println("Ingrese el número de teléfono del cliente: ");
+            String telefono = datos.nextLine();
+
+            // Crear un nuevo cliente y agregarlo a la lista
+            Cliente cliente = new Cliente(id, codigo, nombre, apellido, email, telefono);
+            listaCliente.add(cliente);
+            id++;
+
+            System.out.print("¿Deseas ingresar otro cliente? (S/N): ");
+            decision = datos.nextLine();
+        } while (decision.equalsIgnoreCase("s"));
+
+        System.out.println("\nClientes registrados:");
+        Iterator<Cliente> iterator = listaCliente.iterator();
+        while (iterator.hasNext()) {
+            Cliente cliente = iterator.next();
+            System.out.println(cliente.toString());
+        }
+    }
 }
